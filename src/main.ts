66bcd6c2abc8290e90d54f75478c7af7e4a182f4
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2022-04-30 15:26:55 +0200

    add envelop

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { createServer } from '@graphql-yoga/node'
+import {
+  useGenericAuth,
+  ResolveUserFn
+  // ValidateUserFn
+} from '@envelop/generic-auth'
+
 import { schema } from './schema'
 
+type UserType = {
+  id: string
+}
+
+const resolveUserFn: ResolveUserFn<UserType> = async (_context) => {
+  return { id: '101' }
+}
+
 async function main() {
-  const server = createServer({ schema })
+  const server = createServer({
+    schema,
+    plugins: [
+      useGenericAuth({
+        resolveUserFn,
+        mode: 'protect-all'
+      })
+    ]
+  })
   await server.start()
 }
 
